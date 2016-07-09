27a73c7ed23ceecdc484ad0df9cce052774f1a5a
djsegal <djsegal@wisc.edu>
2016-07-09 03:27:27 -0400

    Remove unnecessary comma

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import BodyClassMixin from 'ember-body-class/mixins/body-class';
 
 export default Ember.Route.extend(BodyClassMixin, {
-  classNames: ["layout-column"],
+  classNames: ["layout-column"]
 });
