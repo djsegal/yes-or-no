86c5f684ab57050b9fce5a00d35a3378b251e6ff
djsegal <djsegal@wisc.edu>
2016-07-09 03:21:02 -0400

    Add ember-body-class addon for the dummy app

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import BodyClassMixin from 'ember-body-class/mixins/body-class';
+
+export default Ember.Route.extend(BodyClassMixin, {
+  classNames: ["layout-column"],
+});
